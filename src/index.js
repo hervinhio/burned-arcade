2d5de793a35d8383d7b69bba21bd495b009a3b79
Hervé Mutombo <hervinhioslash@gmail.com>
2021-02-17 00:13:25 +0100

    Web Feature: cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const store = require('./store');
 const StatusCodes = require('./status-codes');
 
